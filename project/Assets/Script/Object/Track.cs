7ac27158f8c46e14b3e939c1fada384271bf6610
koma2961 <koma2961@yahoo.co.jp>
2014-08-03 02:34:46 +0900

    トラックプレハブを追加しました。

---
+++ project/Assets/Script/Object/Track.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Track : MonoBehaviour {
+	// 速度
+	public Vector2 SPEED = new Vector2(0.05f, 0.0f);
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

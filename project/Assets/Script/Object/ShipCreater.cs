5c76f98eb9843686556e0f744e0e057f36df1754
koma2961 <koma2961@yahoo.co.jp>
2014-08-03 11:32:07 +0900

    飛行船と看板を生成するスクリプトから出るエラーを解決しました。

--- project/Assets/Script/Object/ShipCreater.cs
+++ project/Assets/Script/Object/ShipCreater.cs
@@ -4,19 +4,7 @@ using System.Collections;
 public class ShipCreater : MonoBehaviour
 {
 	
-	// PlayerBulletプレハブ
-	public GameObject bullet;
-	
-	// Startメソッドをコルーチンとして呼び出す
-	IEnumerator Start ()
-	{
-		while (true) {
-			// 弾をプレイヤーと同じ位置/角度で作成
-			Instantiate (bullet, transform.position, transform.rotation);
-			// 10秒待つ
-			yield return new WaitForSeconds (2.0f);
-		}
-	}
+
 	
 	void Update ()
 	{

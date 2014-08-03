cdf3d5addd1a732faaf8a6d5d0e74194e2c3c8fd
koma2961 <koma2961@yahoo.co.jp>
2014-08-03 10:18:47 +0900

    飛行船のプレハブと看板のプレハブを栄背景、名古屋城背景に追加しました。ご確認ください。

---
+++ project/Assets/Script/Object/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+// Rigidbody2Dコンポーネントを必須にする
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+	public float speed = 2.0f;
+	
+	void Start ()
+	{
+		rigidbody2D.velocity = transform.right.normalized * -1.0f * speed;
+	}
+}
---
+++ project/Assets/Script/Object/ShipCreater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipCreater : MonoBehaviour
+{
+	
+	// PlayerBulletプレハブ
+	public GameObject bullet;
+	
+	// Startメソッドをコルーチンとして呼び出す
+	IEnumerator Start ()
+	{
+		while (true) {
+			// 弾をプレイヤーと同じ位置/角度で作成
+			Instantiate (bullet, transform.position, transform.rotation);
+			// 10秒待つ
+			yield return new WaitForSeconds (2.0f);
+		}
+	}
+	
+	void Update ()
+	{
+
+	}
+}
